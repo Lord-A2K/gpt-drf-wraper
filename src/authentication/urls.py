e4db324a3054aec14910fbd0a6aeaaa1f590939e
Lord-A2K <amirkiankaveh@gmail.com>
2024-07-30 19:39:04 +0330

    lord-a2k/fix
    fix refresh token endpoint from token/refresh to refresh/

--- src/authentication/urls.py
+++ src/authentication/urls.py
@@ -8,7 +8,7 @@ from .views import RegisterView
 
 urlpatterns = [
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("register/", RegisterView.as_view(), name="register"),
     path("login/", TokenObtainPairView.as_view(), name="login"),
 ]
