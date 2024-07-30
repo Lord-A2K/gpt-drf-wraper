6d8f55d0b4ac4276316b1bd09a039e12a0fbf6a7
Lord-A2K <amirkiankaveh@gmail.com>
2024-07-30 19:33:09 +0330

    lord-a2k/fix
    change auth endpoint to api/auth

--- src/urls.py
+++ src/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("auth/", include("src.authentication.urls")),
+    path("api/auth/", include("src.authentication.urls")),
     path("api/", include("src.api.urls")),
 ]
